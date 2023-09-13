5de15c49a0e0c5b2935db90570b9689faeca0d2f
vish9151 <84173784+vishal9151@users.noreply.github.com>
2023-09-13 21:30:37 +0530

    first time commit

---
+++ public/chocolates.js
@@ -0,0 +1,50 @@
+export const chocolatesData = [
+  {
+    name: "Dark Chocolate",
+    image: "https://imagetolink.com/ib/wgXMC40vcj.jpg",
+    price: 2.0,
+    quantity: 0
+  },
+  {
+    name: "Milk Chocolate",
+    image: "https://imagetolink.com/ib/HDjSGuBZm7.jpg",
+    price: 1.5,
+    quantity: 0
+  },
+  {
+    name: "White Chocolate",
+    image: "https://imagetolink.com/ib/bVfZltPKTC.jpg",
+    price: 2.5,
+    quantity: 0
+  },
+  {
+    name: "Caramel Chocolate",
+    image: "https://imagetolink.com/ib/gLNFlVC3x9.jpg",
+    price: 3.0,
+    quantity: 0
+  },
+  {
+    name: "Mint Chocolate",
+    image: "https://imagetolink.com/ib/BjRUZrQiOZ.jpg",
+    price: 2.0,
+    quantity: 0
+  },
+  {
+    name: "Hazelnut Chocolate",
+    image: "https://imagetolink.com/ib/TSSHmmzZ7b.jpeg",
+    price: 2.5,
+    quantity: 0
+  },
+  {
+    name: "Almond Chocolate",
+    image: "https://imagetolink.com/ib/SvBISdhLvO.jpg",
+    price: 3.0,
+    quantity: 0
+  },
+  {
+    name: "Coconut Chocolate",
+    image: "https://imagetolink.com/ib/uKQcosgoZM.jpeg",
+    price: 2.5,
+    quantity: 0
+  }
+];
---
+++ src/App.js
@@ -0,0 +1,106 @@
+import "./styles.css";
+import React, { useState, useEffect } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { chocolatesData } from "../public/chocolates"; // A local JSON file that contains the data of the chocolates
+
+function Chocolate({ name, image, price, onIncChange, onDecChange, quantity }) {
+  const increment = () => {
+    onIncChange(name);
+  };
+
+  const decrement = () => {
+    onDecChange(name);
+  };
+
+  return (
+    <div className="chocolate">
+      <img src={image} alt={name} />
+      <h4>{name}</h4>
+      <p>${price}</p>
+      <div className="quantity">
+        <Button variant="outline-dark" onClick={decrement}>
+          -
+        </Button>
+        <span>{quantity}</span>
+        <Button variant="outline-dark" onClick={increment}>
+          +
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+// A component that renders a list of chocolates and allows customers to build a custom pack
+function CustomPack() {
+  // A state that stores the total number of items in the custom pack
+  const [totalItems, setTotalItems] = useState(0);
+
+  // A state that stores the total price of the custom pack
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const [chocolates, setChocolates] = useState(null);
+
+  // A useEffect hook that sets the data of the chocolates from the local JSON file once the component mounts
+  useEffect(() => {
+    setChocolates(chocolatesData);
+  }, []);
+
+  const handleIncChange = (name) => {
+    for (let i = 0; i < chocolates.length; i++) {
+      if (chocolates[i].name === name) {
+        if (totalItems < 8) {
+          chocolates[i].quantity = chocolates[i].quantity + 1;
+          setTotalItems(totalItems + 1);
+          setTotalPrice(totalPrice + chocolates[i].price);
+        }
+      }
+    }
+  };
+
+  const handleDecChange = (name) => {
+    for (let i = 0; i < chocolates.length; i++) {
+      if (chocolates[i].name === name) {
+        if (totalItems > 0 && chocolates[i].quantity > 0) {
+          chocolates[i].quantity = chocolates[i].quantity - 1;
+          setTotalItems(totalItems - 1);
+          setTotalPrice(totalPrice - chocolates[i].price);
+        }
+      }
+    }
+  };
+
+  return (
+    <Container className="custom-pack">
+      <h1>Build Your Custom Pack</h1>
+      <p>Select a combination of 8 chocolates for your custom pack.</p>
+      <Row>
+        {chocolatesData.map((chocolate) => (
+          <Col md={3} key={chocolate.name}>
+            <Chocolate
+              name={chocolate.name}
+              image={chocolate.image}
+              price={chocolate.price}
+              onIncChange={handleIncChange}
+              onDecChange={handleDecChange}
+              quantity={chocolate.quantity}
+            />
+          </Col>
+        ))}
+      </Row>
+      <div className="summary">
+        <p>Total Items: {totalItems}</p>
+        <p>Total Price: ${totalPrice.toFixed(2)}</p>
+        {totalItems === 8 ? (
+          <Button variant="success">Proceed to Checkout</Button>
+        ) : (
+          <p>
+            You need to {totalItems > 8 ? "remove" : "add"}{" "}
+            {Math.abs(8 - totalItems)} more items to your custom pack.
+          </p>
+        )}
+      </div>
+    </Container>
+  );
+}
+
+export default CustomPack;
